bf1dd2f124e529b2da6baeafedbaa2a7e2b2896d
Yu-Xi Lim <limyuxi@gmail.com>
2023-04-08 18:21:26 +0800

    Improve previews

--- fzf-alias.plugin.zsh
+++ fzf-alias.plugin.zsh
@@ -1,9 +1,15 @@
 #!/usr/bin/env zsh
 
+FZF_ALIAS_OPTS=${FZF_ALIAS_OPTS:-"--preview-window up:3:hidden:wrap"}
+
 function fzf_alias() {
     local selection
     # use sed with column to work around MacOS/BSD column not having a -l option
-    if selection=$(alias | sed 's/=/\t/' | column -s '	' -t | fzf --query="$BUFFER" | awk '{ print $1 }'); then
+    if selection=$(alias |
+                       sed 's/=/\t/' |
+                       column -s '	' -t |
+                       FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS $FZF_ALIAS_OPTS" fzf --preview "echo {2..}" --query="$BUFFER" |
+                       awk '{ print $1 }'); then
         BUFFER=$selection
     fi
     zle redisplay
